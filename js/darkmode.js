e2a8ca863caa1e594b374fcc4656e036d64b6e12
nekowawolf <irukainu20@gmail.com>
2024-08-24 23:52:15 +0700

    fix: dark mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,19 +1,16 @@
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+const themeSwitch = document.getElementById('theme-switch');
 
 const enableDarkmode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkmode', 'active')
-}
+  document.documentElement.classList.add('darkmode');
+  localStorage.setItem('darkmode', 'active');
+};
 
 const disableDarkmode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
-}
+  document.documentElement.classList.remove('darkmode');
+  localStorage.setItem('darkmode', null);
+};
 
-if(darkmode === "active") enableDarkmode()
-
-themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+themeSwitch.addEventListener('click', () => {
+  let darkmode = localStorage.getItem('darkmode');
+  darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
+});
