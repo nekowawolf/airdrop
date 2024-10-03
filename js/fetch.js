2d17e06bf5493ee91c5616164f71f4041241a340
nekowawolf <irukainu20@gmail.com>
2024-10-03 22:47:48 +0700

    update

--- js/fetch.js
+++
@@ -1,5 +0,0 @@
-import { get } from "https://bukulapak.github.io/api/process.js";
-import { fillTableAirdrop } from "./controller/get.js";
-import { urlAPIFree } from "./config/url.js";
-
-get(urlAPIFree, fillTableAirdrop);
--- js/fetch2.js
+++
@@ -1,5 +0,0 @@
-import { get } from "https://bukulapak.github.io/api/process.js";
-import { fillTableAirdrop } from "./controller/get.js";
-import { urlAPIPaid } from "./config/url.js";
-
-get(urlAPIPaid, fillTableAirdrop);
